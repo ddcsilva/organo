5231cfc1887aef1dcd342ee19a11848dcf3673d3
Danilo Silva <danilo.silva@msn.com>
2025-05-25 19:56:22 -0300

    feat: Adicionar times com cores personalizadas no componente App e atualizar o componente Team para aplicar estilos dinâmicos

--- src/App.js
+++ src/App.js
@@ -2,7 +2,46 @@ import { useState } from "react";
 import Banner from "./components/Banner";
 import Form from "./components/Form";
 import Team from "./components/Team";
+
 function App() {
+  const teams = [
+    {
+      name: "Programação",
+      primaryColor: "#57c278",
+      secondaryColor: "#d9f7e9",
+    },
+    {
+      name: "Front-End",
+      primaryColor: "#82cffa",
+      secondaryColor: "#e8f8ff",
+    },
+    {
+      name: "Data Science",
+      primaryColor: "#a6d157",
+      secondaryColor: "#f0f8e2",
+    },
+    {
+      name: "Devops",
+      primaryColor: "#e06b69",
+      secondaryColor: "#fde7e8",
+    },
+    {
+      name: "UX e Design",
+      primaryColor: "#db6ebf",
+      secondaryColor: "#fae9f5",
+    },
+    {
+      name: "Mobile",
+      primaryColor: "#ffba05",
+      secondaryColor: "#fff5d9",
+    },
+    {
+      name: "Inovação e Gestão",
+      primaryColor: "#ff8a29",
+      secondaryColor: "#ffeedf",
+    },
+  ];
+
   const [collaborators, setCollaborators] = useState([]);
 
   const addCollaborator = (collaborator) => {
@@ -13,9 +52,9 @@ function App() {
     <div className="App">
       <Banner />
       <Form addCollaborator={addCollaborator} />
-      <Team name="Programação" />
-      <Team name="Front-End" />
-      <Team name="Data Science" />
+      {teams.map((team) => (
+        <Team key={team.name} name={team.name} primaryColor={team.primaryColor} secondaryColor={team.secondaryColor} />
+      ))}
     </div>
   );
 }
